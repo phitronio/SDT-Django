6c7646f78eb45eadfe023af27db08132e53e9de2
Romjan Ali <romjanvr5@gmail.com>
2024-12-21 02:04:19 +0600

    Module 6: Django Form & Model Forms

--- task-management/tasks/models.py
+++ task-management/tasks/models.py
@@ -6,7 +6,10 @@ from django.db import models
 class Employee(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    # task_set
+    # tasks
+
+    def __str__(self):
+        return self.name
 
 
 class Task(models.Model):
--- task-management/tasks/views.py
+++ task-management/tasks/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from tasks.forms import TaskForm, TaskModelForm
+from tasks.models import Employee, Task
 
 # Create your views here.
+
 
 def manager_dashboard(request):
     return render(request, "dashboard/manager-dashboard.html")
@@ -9,6 +12,7 @@ def manager_dashboard(request):
 
 def user_dashboard(request):
     return render(request, "dashboard/user-dashboard.html")
+
 
 def test(request):
     names = ["Mahmud", "Ahamed", "John", "Mr. X"]
@@ -21,3 +25,37 @@ def test(request):
         "count": count
     }
     return render(request, 'test.html', context)
+
+
+def create_task(request):
+    # employees = Employee.objects.all()
+    form = TaskModelForm()  # For GET
+
+    if request.method == "POST":
+        form = TaskModelForm(request.POST)
+        if form.is_valid():
+
+            """ For Model Form Data """
+            form.save()
+
+            return render(request, 'task_form.html', {"form": form, "message": "task added successfully"})
+
+            ''' For Django Form Data'''
+            # data = form.cleaned_data
+            # title = data.get('title')
+            # description = data.get('description')
+            # due_date = data.get('due_date')
+            # assigned_to = data.get('assigned_to')  # list [1,3]
+
+            # task = Task.objects.create(
+            #     title=title, description=description, due_date=due_date)
+
+            # # Assign employee to tasks
+            # for emp_id in assigned_to:
+            #     employee = Employee.objects.get(id=emp_id)
+            #     task.assigned_to.add(employee)
+
+            # return HttpResponse("Task Added successfully")
+
+    context = {"form": form}
+    return render(request, "task_form.html", context)
