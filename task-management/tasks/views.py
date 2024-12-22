b5f5e519bb90c60349254297e9c1b41a404a7e3d
Romjan Ali <romjanvr5@gmail.com>
2024-12-22 18:34:05 +0600

    8.2 Optimizing Database Query

--- task-management/tasks/views.py
+++ task-management/tasks/views.py
@@ -10,20 +10,32 @@ from django.db.models import Q, Count, Max, Min, Avg
 
 
 def manager_dashboard(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.select_related(
+        'details').prefetch_related('assigned_to').all()
 
     # getting task count
-    total_task = tasks.count()
-    completed_task = Task.objects.filter(status="COMPLETED").count()
-    in_progress_task = Task.objects.filter(status='IN_PROGRESS').count()
-    pending_task = Task.objects.filter(status="PENDING").count()
+    # total_task = tasks.count()
+    # completed_task = Task.objects.filter(status="COMPLETED").count()
+    # in_progress_task = Task.objects.filter(status='IN_PROGRESS').count()
+    # pending_task = Task.objects.filter(status="PENDING").count()
+
+    # count = {
+    #     "total_task":
+    #     "completed_task":
+    #     "in_progress_task":
+    #     "pending_task":
+    # }
+
+    counts = Task.objects.aggregate(
+        total=Count('id'),
+        completed=Count('id', filter=Q(status='COMPLETED')),
+        in_progress=Count('id', filter=Q(status='IN_PROGRESS')),
+        pending=Count('id', filter=Q(status='PENDING')),
+    )
 
     context = {
         "tasks": tasks,
-        "total_task": total_task,
-        "pending_task": pending_task,
-        "in_progress_task": in_progress_task,
-        "completed_task": completed_task
+        "counts": counts
     }
     return render(request, "dashboard/manager-dashboard.html", context)
 
