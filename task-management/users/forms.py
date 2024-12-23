e0f8d8967736781db253c3388c29a7f67a9d3286
Romjan Ali <romjanvr5@gmail.com>
2024-12-24 01:55:31 +0600

    9.6 Login with custome HTML form

--- task-management/users/forms.py
+++ task-management/users/forms.py
@@ -2,6 +2,7 @@ from django import forms
 import re
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from tasks.forms import StyledFormMixin
 
 
 class RegisterForm(UserCreationForm):
@@ -17,7 +18,7 @@ class RegisterForm(UserCreationForm):
             self.fields[fieldname].help_text = None
 
 
-class CustomRegistrationForm(forms.ModelForm):
+class CustomRegistrationForm(StyledFormMixin, forms.ModelForm):
     password1 = forms.CharField(widget=forms.PasswordInput)
     confirm_password = forms.CharField(widget=forms.PasswordInput)
 
@@ -26,6 +27,15 @@ class CustomRegistrationForm(forms.ModelForm):
         fields = ['username', 'first_name', 'last_name',
                   'password1', 'confirm_password', 'email']
 
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        email_exists = User.objects.filter(email=email).exists()
+
+        if email_exists:
+            raise forms.ValidationError("Email already exists")
+
+        return email
+
     def clean_password1(self):
         password1 = self.cleaned_data.get('password1')
         errors = []
@@ -33,8 +43,20 @@ class CustomRegistrationForm(forms.ModelForm):
         if len(password1) < 8:
             errors.append('Password must be at least 8 character long')
 
-        if "abc" not in password1:
-            errors.append("Password must include abc")
+        if not re.search(r'[A-Z]', password1):
+            errors.append(
+                'Password must include at least one uppercase letter.')
+
+        if not re.search(r'[a-z]', password1):
+            errors.append(
+                'Password must include at least one lowercase letter.')
+
+        if not re.search(r'[0-9]', password1):
+            errors.append('Password must include at least one number.')
+
+        if not re.search(r'[@#$%^&+=]', password1):
+            errors.append(
+                'Password must include at least one special character.')
 
         if errors:
             raise forms.ValidationError(errors)
@@ -46,7 +68,7 @@ class CustomRegistrationForm(forms.ModelForm):
         password1 = cleaned_data.get('password1')
         confirm_password = cleaned_data.get('confirm_password')
 
-        if password1 != confirm_password:
+        if password1 and confirm_password and password1 != confirm_password:
             raise forms.ValidationError("Password do not match")
 
         return cleaned_data
--- task-management/users/views.py
+++ task-management/users/views.py
@@ -1,26 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
 from users.forms import CustomRegistrationForm
 
 # Create your views here.
 
 
 def sign_up(request):
-    if request.method == 'GET':
-        form = CustomRegistrationForm()
+    form = CustomRegistrationForm()
     if request.method == 'POST':
         form = CustomRegistrationForm(request.POST)
         if form.is_valid():
-            # username = form.cleaned_data.get('username')
-            # password = form.cleaned_data.get('password1')
-            # confirm_password = form.cleaned_data.get('password2')
-
-            # if password == confirm_password:
-            #     User.objects.create(username=username, password=password)
-            # else:
-            #     print("Password are not same")
             form.save()
         else:
             print("Form is not valid")
     return render(request, 'registration/register.html', {"form": form})
+
+
+def sign_in(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print("Doc", username, password)
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+    return render(request, 'registration/login.html')
