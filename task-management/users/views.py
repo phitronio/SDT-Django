5f5355374514dc8634854c202a8c507fff24a9e1
Romjan Ali <romjanvr5@gmail.com>
2024-12-31 00:16:28 +0600

    Module 12.8 Optimize query for fetching userlist and grouplist

--- task-management/users/views.py
+++ task-management/users/views.py
@@ -7,6 +7,7 @@ from django.contrib import messages
 from users.forms import LoginForm
 from django.contrib.auth.tokens import default_token_generator
 from django.contrib.auth.decorators import login_required, user_passes_test
+from django.db.models import Prefetch
 
 
 # Create your views here.
@@ -44,6 +45,7 @@ def sign_in(request):
             return redirect('home')
     return render(request, 'registration/login.html', {'form': form})
 
+
 @login_required
 def sign_out(request):
     if request.method == 'POST':
@@ -64,25 +66,38 @@ def activate_user(request, user_id, token):
     except User.DoesNotExist:
         return HttpResponse('User not found')
 
+
 @user_passes_test(is_admin, login_url='no-permission')
 def admin_dashboard(request):
-    users = User.objects.all()
+    users = User.objects.prefetch_related(
+        Prefetch('groups', queryset=Group.objects.all(), to_attr='all_groups')
+    ).all()
+
+    print(users)
+
+    for user in users:
+        if user.all_groups:
+            user.group_name = user.all_groups[0].name
+        else:
+            user.group_name = 'No Group Assigned'
     return render(request, 'admin/dashboard.html', {"users": users})
+
 
 @user_passes_test(is_admin, login_url='no-permission')
 def assign_role(request, user_id):
     user = User.objects.get(id=user_id)
     form = AssignRoleForm()
-    
+
     if request.method == 'POST':
         form = AssignRoleForm(request.POST)
         if form.is_valid():
             role = form.cleaned_data.get('role')
-            user.groups.clear() # Remove old roles
+            user.groups.clear()  # Remove old roles
             user.groups.add(role)
-            messages.success(request, f"User {user.username} has been assigned to the {role.name} role")
+            messages.success(request, f"User {
+                             user.username} has been assigned to the {role.name} role")
             return redirect('admin-dashboard')
-    
+
     return render(request, 'admin/assign_role.html', {"form": form})
 
 
@@ -91,19 +106,22 @@ def create_group(request):
     form = CreateGroupForm()
     if request.method == 'POST':
         form = CreateGroupForm(request.POST)
-        
+
         if form.is_valid():
             group = form.save()
-            messages.success(request, f"Group {group.name} has been created successfully")
+            messages.success(request, f"Group {
+                             group.name} has been created successfully")
             return redirect('create-group')
-        
+
     return render(request, 'admin/create_group.html', {'form': form})
+
 
 @user_passes_test(is_admin, login_url='no-permission')
 def group_list(request):
-    groups = Group.objects.all()
+    groups = Group.objects.prefetch_related('permissions').all()
     return render(request, 'admin/group_list.html', {'groups': groups})
-            
+
+
 """ 
 
     Admin
