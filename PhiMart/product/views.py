1b32c28acaa1e0e932583bbc3a12fd79d563946e
Romjan Ali <romjanvr5@gmail.com>
2025-01-22 02:25:55 +0600

    Module 20-1 to 20-4 added

--- PhiMart/product/views.py
+++ PhiMart/product/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from product.models import Product, Category
+from product.serializers import ProductSerializer, CategorySerializer
 # Create your views here.
+
+
+@api_view()
+def view_products(request):
+    products = Product.objects.select_related('category').all()
+    serializer = ProductSerializer(
+        products, many=True, context={'request': request})
+    return Response(serializer.data)
+
+
+@api_view()
+def view_specific_product(request, id):
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+
+@api_view()
+def view_categories(request):
+    return Response({'message': 'Categories'})
+
+
+@api_view()
+def view_specific_category(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    serializer = CategorySerializer(category)
+    return Response(serializer.data)
