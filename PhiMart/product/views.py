d55acafa8a1064681dc88e84d3634ef39f433def
Romjan Ali <romjanvr5@gmail.com>
2025-01-24 23:46:39 +0600

    Module 20 added

--- PhiMart/product/views.py
+++ PhiMart/product/views.py
@@ -5,27 +5,52 @@ from rest_framework.response import Response
 from rest_framework import status
 from product.models import Product, Category
 from product.serializers import ProductSerializer, CategorySerializer
+from django.db.models import Count
 # Create your views here.
 
 
-@api_view()
+@api_view(['GET', 'POST'])
 def view_products(request):
-    products = Product.objects.select_related('category').all()
-    serializer = ProductSerializer(
-        products, many=True, context={'request': request})
-    return Response(serializer.data)
+    if request.method == 'GET':
+        products = Product.objects.select_related('category').all()
+        serializer = ProductSerializer(
+            products, many=True)
+        return Response(serializer.data)
+    if request.method == 'POST':
+        # Deserializer
+        serializer = ProductSerializer(
+            data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view()
+@api_view(['GET', 'PUT', 'DELETE'])
 def view_specific_product(request, id):
-    product = get_object_or_404(Product, pk=id)
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        product = get_object_or_404(Product, pk=id)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    if request.method == 'PUT':
+        product = get_object_or_404(Product, pk=id)
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    if request.method == 'DELETE':
+        product = get_object_or_404(Product, pk=id)
+        copy_of_product = product
+        product.delete()
+        serializer = ProductSerializer(copy_of_product)
+        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view()
 def view_categories(request):
-    return Response({'message': 'Categories'})
+    categories = Category.objects.annotate(
+        product_count=Count('products')).all()
+    serializer = CategorySerializer(categories, many=True)
+    return Response(serializer.data)
 
 
 @api_view()
