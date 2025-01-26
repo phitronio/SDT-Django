15bf2585c63d506f98b5aebef8e7ac5d4a577e16
Romjan Ali <romjanvr5@gmail.com>
2025-01-26 15:41:19 +0600

    Module 21.6 added

--- PhiMart/api/urls.py
+++ PhiMart/api/urls.py
@@ -1,15 +1,18 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from product.views import ProductViewSet, CategoryViewSet
+from product.views import ProductViewSet, CategoryViewSet, ReviewViewSet
+from rest_framework_nested import routers
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register('products', ProductViewSet)
 router.register('categories', CategoryViewSet)
+
+product_router = routers.NestedDefaultRouter(
+    router, 'products', lookup='product')
+product_router.register('reviews', ReviewViewSet, basename='product-review')
 
 # urlpatterns = router.urls
 
 urlpatterns = [
     path('', include(router.urls)),
-    # aro urls thake segulo ekhane dewa jabe
-    # path('contact/')
+    path('', include(product_router.urls))
 ]
--- PhiMart/product/serializers.py
+++ PhiMart/product/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from decimal import Decimal
-from product.models import Category, Product
+from product.models import Category, Product, Review
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -59,3 +59,13 @@ class ProductSerializer(serializers.ModelSerializer):
     # def validate(self, attrs):
     #     if attrs['password1'] != attrs['password2']:
     #         raise serializers.ValidationError("Password didn't pass")
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ['id', 'name', 'description']
+
+    def create(self, validated_data):
+        product_id = self.context['product_id']
+        return Review.objects.create(product_id=product_id, **validated_data)
