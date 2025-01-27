5e22ef9bd8ff2cff651cbe5244e48b2091e96b63
Romjan Ali <romjanvr5@gmail.com>
2025-01-27 20:13:32 +0600

    Module 22.3 Added

--- PhiMart/order/serializers.py
+++ PhiMart/order/serializers.py
@@ -1,8 +1,37 @@
 from rest_framework import serializers
 from order.models import Cart, CartItem
+from product.models import Product
+from product.serializers import ProductSerializer
+
+
+class SimpleProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'price']
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product = SimpleProductSerializer()
+    total_price = serializers.SerializerMethodField(
+        method_name='get_total_price')
+
+    class Meta:
+        model = CartItem
+        fields = ['id', 'product', 'quantity', 'product', 'total_price']
+
+    def get_total_price(self, cart_item: CartItem):
+        return cart_item.quantity * cart_item.product.price
 
 
 class CartSerializer(serializers.ModelSerializer):
+    items = CartItemSerializer(many=True)
+    total_price = serializers.SerializerMethodField(
+        method_name='get_total_price')
+
     class Meta:
         model = Cart
-        fields = ['id', 'user']
+        fields = ['id', 'user', 'items', 'total_price']
+
+    def get_total_price(self, cart: Cart):
+        return sum(
+            [item.product.price * item.quantity for item in cart.items.all()])
