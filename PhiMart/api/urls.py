958e8a176eb47d56a061e328ca92a2ad0347df0a
Romjan Ali <romjanvr5@gmail.com>
2025-01-27 19:40:37 +0600

    Module 22.1 & 22.2 added

--- PhiMart/api/urls.py
+++ PhiMart/api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from product.views import ProductViewSet, CategoryViewSet, ReviewViewSet
+from order.views import CartViewSet
 from rest_framework_nested import routers
 
 router = routers.DefaultRouter()
 router.register('products', ProductViewSet, basename='products')
 router.register('categories', CategoryViewSet)
+router.register('carts', CartViewSet, basename='carts')
 
 product_router = routers.NestedDefaultRouter(
     router, 'products', lookup='product')
