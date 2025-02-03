bf1ef49b3fbfbe006fe0992e3aebe947a8e6abc4
Romjan Ali <romjanvr5@gmail.com>
2025-02-04 00:14:51 +0600

    Module 24.1 to 24.5 added

--- PhiMart/order/admin.py
+++ PhiMart/order/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from order.models import Cart, CartItem
+from order.models import Cart, CartItem, Order, OrderItem
 
 # Register your models here.
 
@@ -9,4 +9,10 @@ class CartAdmin(admin.ModelAdmin):
     list_display = ['id', 'user']
 
 
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'status']
+
+
 admin.site.register(CartItem)
+admin.site.register(OrderItem)
--- PhiMart/order/views.py
+++ PhiMart/order/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
-from order.serializers import CartSerializer, CartItemSerializer, AddCartItemSerializer, UpdateCartItemSerializer
-from order.models import Cart, CartItem
+from order.serializers import CartSerializer, CartItemSerializer, AddCartItemSerializer, UpdateCartItemSerializer, OrderSerializer, CreateOrderSerializer
+from order.models import Cart, CartItem, Order, OrderItem
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
+
+# serializer = OrderSerializer(order)
+# return Response(serializer)
 
 
 class CartViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, GenericViewSet):
-    queryset = Cart.objects.all()
     serializer_class = CartSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        return Cart.objects.prefetch_related('items__product').filter(user=self.request.user)
 
 
 class CartItemViewSet(ModelViewSet):
@@ -25,4 +35,21 @@ class CartItemViewSet(ModelViewSet):
         return {'cart_id': self.kwargs['cart_pk']}
 
     def get_queryset(self):
-        return CartItem.objects.filter(cart_id=self.kwargs['cart_pk'])
+        return CartItem.objects.select_related('product').filter(cart_id=self.kwargs['cart_pk'])
+
+
+class OrderViewset(ModelViewSet):
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return CreateOrderSerializer
+        return OrderSerializer
+
+    def get_serializer_context(self):
+        return {'user_id': self.request.user.id}
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Order.objects.prefetch_related('items__product').all()
+        return Order.objects.prefetch_related('items__product').filter(user=self.request.user)
