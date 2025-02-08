e473ca221400181e2a98b2a676dfd405cfd1d703
Romjan Ali <romjanvr5@gmail.com>
2025-02-09 00:58:59 +0600

    25.5 to 25.8 added

--- PhiMart/order/views.py
+++ PhiMart/order/views.py
@@ -18,6 +18,8 @@ class CartViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, Gener
         serializer.save(user=self.request.user)
 
     def get_queryset(self):
+        if getattr(self, 'swagger_fake_view', False):
+            return Cart.objects.none()
         return Cart.objects.prefetch_related('items__product').filter(user=self.request.user)
 
 
@@ -32,10 +34,14 @@ class CartItemViewSet(ModelViewSet):
         return CartItemSerializer
 
     def get_serializer_context(self):
-        return {'cart_id': self.kwargs['cart_pk']}
+        context = super().get_serializer_context()
+        if getattr(self, 'swagger_fake_view', False):
+            return context
+
+        return {'cart_id': self.kwargs.get('cart_pk')}
 
     def get_queryset(self):
-        return CartItem.objects.select_related('product').filter(cart_id=self.kwargs['cart_pk'])
+        return CartItem.objects.select_related('product').filter(cart_id=self.kwargs.get('cart_pk'))
 
 
 class OrderViewset(ModelViewSet):
@@ -71,9 +77,13 @@ class OrderViewset(ModelViewSet):
         return orderSz.OrderSerializer
 
     def get_serializer_context(self):
+        if getattr(self, 'swagger_fake_view', False):
+            return super().get_serializer_context()
         return {'user_id': self.request.user.id, 'user': self.request.user}
 
     def get_queryset(self):
+        if getattr(self, 'swagger_fake_view', False):
+            return Order.objects.none()
         if self.request.user.is_staff:
             return Order.objects.prefetch_related('items__product').all()
         return Order.objects.prefetch_related('items__product').filter(user=self.request.user)
