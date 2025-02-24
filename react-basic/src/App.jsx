37398f0296727b1d21400b42e8d75acb5cf7f6c6
Romjan Ali <romjanvr5@gmail.com>
2025-02-25 00:49:31 +0600

    30.1 & 30.3 added

---
+++ react-basic/src/App.jsx
@@ -0,0 +1,11 @@
+import CartApp from "./cart/CartApp";
+
+function App() {
+  return (
+    <div className="m-5">
+      <CartApp />
+    </div>
+  );
+}
+
+export default App;
--- react-basic/src/main.jsx
+++ react-basic/src/main.jsx
@@ -1,13 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
 import "./index.css";
-import AppRoutes from "./routes/AppRoutes.jsx";
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
